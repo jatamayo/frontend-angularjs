d95a914fc2df7efa2d9d38ce3084465ea8753c84
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-01-22 18:48:10 -0600

    primera modificacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 // Javascript Code.
+(function (){
+	var app=angular.module("frontendAngularjs",[]);
+	.service("response"($http){
+		this.getAll=function(success, failure){}
+			$http.get("nombre de la appi")
+				.success(success)
+				.error(failure);
+	})
+	.controller("controllerAngularjs",function($scope,response){
+		$scope.name="carl";
+		response.getAll(function(data){
+			$scope.response=data.response;
+		});
+		});
+	});
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
