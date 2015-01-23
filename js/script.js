44ab404558b0d09cd69a729108d9824f346e94ae
Jose Alejandro Tamayo <jalejandrotamayo@hotmail.com>
2015-01-23 11:18:58 -0600

    llamada de appi last fm

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 (function(){
 	var app=angular.module("angularjs",[]);
 /******************controlador ejemplo**************************/
-	app.controller("informacion",function(){
-		this.datos=datos;
-	})
-	var datos=	{
-				nombre:"jose",
-				apellido:"tamayo",
-				edad:22
-				};
+	
 /***************************************************************/
 /******************JSON DE PAISES COMO EJEMPLO******************/
 	app.controller("paises",function($scope,$http){
 		//$scope.name="jose";
-		$http.get("data/paises.json").success(function(data){
-			$scope.informacion=data;
+		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=39b875f4c487778289f6bb6a8dc75c24&format=json").success(function(data){
+			$scope.informacion=data.tracks.track;
 		});
 	});
 /**************************************************************/
