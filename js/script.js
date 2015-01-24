3a3d99f121e41d8e00b45c1929ed29362bb60236
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-01-23 18:18:02 -0600

    prototipo de filtros terminado uno

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@
 	});
 	var info={
 			nombre:"jose",
-			edad:22
+			edad:22,
+			apellido:"tamayo"
 			}
 /***************************************************************/
 /******************JSON DE PAISES COMO EJEMPLO******************/
-	app.controller("paises",function($scope,$http){
+	app.controller("lastfm",function($scope,$http){
 		//$scope.name="jose";
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=39b875f4c487778289f6bb6a8dc75c24&format=json").success(function(data){
 			$scope.informacion=data.tracks.track;
