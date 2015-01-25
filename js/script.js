d99e9740049c58ce75d887cfde79f0d7aa9e8de1
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-01-24 23:08:08 -0600

    filtros por botones terminados

--- js/script.js
+++ js/script.js
@@ -1,16 +1,8 @@
 (function(){
 	var app=angular.module("angularjs",[]);
-/******************controlador ejemplo**************************/
-	app.controller("tamayo",function(){
-		this.regresodatos=info;
-	});
-	var info={
-			nombre:"jose",
-			edad:22,
-			apellido:"tamayo"
-			}
+
 /***************************************************************/
-/******************JSON DE PAISES COMO EJEMPLO******************/
+/******************LLAMADO DE APPI LASTFM******************/
 	app.controller("lastfm",function($scope,$http){
 		//$scope.name="jose";
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=39b875f4c487778289f6bb6a8dc75c24&format=json").success(function(data){
