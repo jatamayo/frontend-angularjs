ac61f4916dc5538f3f2559c19a7cee83ac5f95aa
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-01-25 01:05:20 -0600

    terminado sin mayor presentacion

--- js/script.js
+++ js/script.js
@@ -3,12 +3,20 @@
 
 /***************************************************************/
 /******************LLAMADO DE APPI LASTFM******************/
+	app.filter("soloUrl", function(){
+	return function(item){
+		return (JSON.stringify(item)).slice(10,-18);
+		};
+	});
+
 	app.controller("lastfm",function($scope,$http){
 		//$scope.name="jose";
 		
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=39b875f4c487778289f6bb6a8dc75c24&format=json").success(function(data){
 			$scope.informacion=data.tracks.track;
+
 		});
+		
 		
 	});
 /**************************************************************/
