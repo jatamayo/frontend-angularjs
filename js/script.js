f58ee083ed0a29e4bc3c79ec39e27f2b06e3cdfa
alejandro tamayo <jalejandrotamayo@hotmail.com>
2015-01-24 23:45:52 -0600

    filtro por palabras terminado

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@
 /******************LLAMADO DE APPI LASTFM******************/
 	app.controller("lastfm",function($scope,$http){
 		//$scope.name="jose";
+		
 		$http.get("http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=39b875f4c487778289f6bb6a8dc75c24&format=json").success(function(data){
 			$scope.informacion=data.tracks.track;
 		});
+		
 	});
 /**************************************************************/
 })();
